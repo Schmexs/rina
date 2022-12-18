e239298c9663a19b109ce83d8d6aba60b16e3893
Maximilian <maximilian.brandt@mailbox.tu-dresden.de>
2022-12-18 18:16:53 +0100

    Add drop_package script with qdisc

---
+++ rina_package_drop.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+
+import subprocess
+import rina
+import logging
+import time
+import re
+
+lo = logging.getLogger("rina")
+
+# Line
+line_result_rina = {}
+line_result_ip = {}
+
+rinaper_re = re.compile(r"Sender\s*\d*\s*[\d\.]*\s*([\d\.]*)")
+
+def line_datarate_test():
+    global line_result_rina
+    global line_result_ip
+
+    test_sizes = [2, 5]
+    #test_sizes = [2, 5, 10, 30, 50]
+
+    for size in test_sizes:
+        lo.info(f"Running datarate test with line topology with {size} nodes.")
+        rina.cleanup()
+        rina.create_networknamespaces(size)
+        rina.create_line_topology(size)
+        time.sleep(size)
+        package_loss = 0.5
+        rina.run('tc', 'qdisc', 'add', 'dev', f'veth{size - 1}-{size - 2}', 'root', 'netem', 'loss', str(package_loss)+'%', netns=f'node{size - 1}')
+        
+        rina.run('bash', '-c', 'rinaperf -l -d n.DIF &', netns='node0')
+        rina.run('netserver', netns='node0')
+        while package_loss <= 10:
+            rina.run('tc', 'qdisc', 'change', 'dev', f'veth{size - 1}-{size - 2}', 'root', 'netem', 'loss', str(package_loss)+'%', netns=f'node{size - 1}')
+           
+            try:
+                rina_result_raw = rina.run('rinaperf', '-d', 'n.DIF', '-t', 'perf', '-s', '1460', '-D', '10', netns=f'node{size - 1}', stdout=subprocess.PIPE)
+            except RuntimeError as e:
+                print(f"rinaperf failed: {e}")
+                rina_result_raw = ""
+
+            match = re.search(rinaper_re, rina_result_raw)
+
+            if match is None:
+                print(f"No valid result from rinaperf: {rina_result_raw}")
+                line_result_rina[f'{size}:{package_loss}'] = 0
+            else:
+                line_result_rina[f'{size}:{package_loss}'] = float(match.group(1))
+
+            
+            ip_result_raw =  rina.run('netperf', '-H', '10.0.0.1', '-P', '0', '--', '-o', 'THROUGHPUT', '--', '-m', '1460', netns=f'node{size - 1}', stdout=subprocess.PIPE)
+            line_result_ip[f'{size}:{package_loss}'] = float(ip_result_raw.strip())
+            package_loss += 0.5
+
+
+# Fully meshed
+mesh_result_rina = {}
+mesh_result_ip = {}
+
+def mesh_datarate_test():
+    global mesh_result_rina
+    global mesh_result_ip
+
+    test_sizes = [3, 6]
+    #test_sizes = [2, 5, 10, 30, 50]
+
+    for size in test_sizes:
+        lo.info(f"Running datarate test with fully meshed topology with {size} nodes.")
+
+        rina.cleanup()
+        rina.create_networknamespaces(size)
+        rina.create_fully_meshed_topology(size)
+        time.sleep(size * 3)
+        package_loss = 0.5
+        rina.run('tc', 'qdisc', 'add', 'dev', f'veth{size - 1}-{size - 2}', 'root', 'netem', 'loss', str(package_loss)+'%', netns=f'node{size - 1}')
+        
+        rina.run('bash', '-c', 'rinaperf -l -d n.DIF &', netns='node0')
+        rina.run('netserver', netns='node0')
+
+        while package_loss <= 10:
+            rina.run('tc', 'qdisc', 'change', 'dev', f'veth{size - 1}-{size - 2}', 'root', 'netem', 'loss', str(package_loss)+'%', netns=f'node{size - 1}')
+
+            try:
+                rina_result_raw = rina.run('rinaperf', '-d', 'n.DIF', '-t', 'perf', '-s', '1460', '-D', '10', netns=f'node{size - 1}', stdout=subprocess.PIPE)
+            except RuntimeError as e:
+                print(f"rinaperf failed: {e}")
+                rina_result_raw = ""
+
+            match = re.search(rinaper_re, rina_result_raw)
+
+            if match is None:
+                print(f"No valid result from rinaperf: {rina_result_raw}")
+                line_result_rina[f'{size}:{package_loss}'] = 0
+            else:
+                line_result_rina[f'{size}:{package_loss}'] = float(match.group(1))
+
+            ip_result_raw =  rina.run('netperf', '-H', f'10.0.{size-1}.1', '-P', '0', '--', '-o', 'THROUGHPUT', '--', '-m', '1460', netns=f'node{size - 1}', stdout=subprocess.PIPE)
+            mesh_result_ip[f'{size}:{package_loss}'] = float(ip_result_raw.strip())
+            package_loss += 0.5
+
+
+
+def main():
+    rina.load_rlite()
+    line_datarate_test()
+    mesh_datarate_test()
+    rina.cleanup()
+
+    print("RESULTS: (in MBit/s)")
+    print("LINE")
+    print(f"TCP/IP: {line_result_ip}")
+    print(f"RINA: {line_result_rina}")
+    print("\n")
+    print("MESH:")
+    print(f"TCP/IP: {mesh_result_ip}")
+    print(f"RINA: {mesh_result_rina}")
+
+
+if __name__ == "__main__":
+    main()
