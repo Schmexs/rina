d9f93ca5601c4981c55871c35d03d0382f04d841
Jakob Mueller <jakob.mueller@ibot3.de>
2023-02-08 23:01:41 +0100

    Add TCP reno test, use cubic as default

--- datarate.py
+++ datarate.py
@@ -13,27 +13,32 @@ rinaper_re = re.compile(r"Receiver\s*\d*\s*[\d\.]*\s*([\d\.]*)")
 def match_rina_result(result):
     return float(re.search(rinaper_re, result).group(1))
 
-def datarate_test(from_ns, to_ns, to_ip):
+def datarate_test(from_ns, to_ns, to_ip, tcp_congestion_algo='cubic', skip_rina=False):
     rina.run('bash', '-c', 'rinaperf -l -d n.DIF &', netns=to_ns)
     rina.run('bash', '-c', 'iperf3 -s &', netns=to_ns)
 
     results_rina = []
     results_ip = []
 
-    timeout_try_rina = 0
-    success_try_rina = 0
-    while success_try_rina < SAMPLES:
-        if timeout_try_rina > RETIRES_MAX:
-            results_rina.append(0)
-            break
-        try:
-            rina_result_raw = rina.run('rinaperf', '-d', 'n.DIF', '-t', 'perf', '-s', '1460', '-D', '5', '-g', '0', netns=from_ns, stdout=subprocess.PIPE)
-            results_rina.append(match_rina_result(rina_result_raw))
-            success_try_rina += 1
-        except RuntimeError as e:
-            print(f"rinaperf failed: {e}")
-            timeout_try_rina += 1
-            #results_rina.append(0)
+    result_rina = None
+
+    if not skip_rina:
+        timeout_try_rina = 0
+        success_try_rina = 0
+        while success_try_rina < SAMPLES:
+            if timeout_try_rina > RETIRES_MAX:
+                results_rina.append(0)
+                break
+            try:
+                rina_result_raw = rina.run('rinaperf', '-d', 'n.DIF', '-t', 'perf', '-s', '1460', '-D', '5', '-g', '0', netns=from_ns, stdout=subprocess.PIPE)
+                results_rina.append(match_rina_result(rina_result_raw))
+                success_try_rina += 1
+            except RuntimeError as e:
+                print(f"rinaperf failed: {e}")
+                timeout_try_rina += 1
+                #results_rina.append(0)
+        
+        result_rina = round(statistics.mean(results_rina))
 
     timeout_try_ip = 0
     success_try_ip = 0
@@ -42,7 +47,7 @@ def datarate_test(from_ns, to_ns, to_ip):
             results_ip.append(0)
             break
         try :
-            ip_result_raw =  rina.run('iperf3', '-c', to_ip, '-J', '-t', '5', '-M', '1460', netns=from_ns, stdout=subprocess.PIPE)
+            ip_result_raw =  rina.run('iperf3', '-c', to_ip, '-J', '-t', '5', '-M', '1460', '-C', tcp_congestion_algo, netns=from_ns, stdout=subprocess.PIPE)
             ip_result_dict = json.loads(ip_result_raw)
             results_ip.append(ip_result_dict['end']["sum_received"]['bits_per_second'] / 10 ** 6)
             success_try_ip += 1
@@ -52,7 +57,6 @@ def datarate_test(from_ns, to_ns, to_ip):
             print(f"iperf3 failed: {e}")
 
     result_ip = round(statistics.mean(results_ip))
-    result_rina = round(statistics.mean(results_rina))
 
     rina.run('killall', 'rinaperf', netns=to_ns)
     rina.run('killall', 'iperf3', netns=to_ns)
--- rina_package_drop.py
+++ rina_package_drop.py
@@ -15,8 +15,12 @@ LOSS_INCREASE = 0.05
 LOSS_MAX = 0.3
 SAMPLES = 4
 RETIRES_MAX = 10
+LINE_SIZES = [2, 5, 10, 20]
+MESH_SIZES = [5, 10, 30]
+REDUNDANT_SIZES = [5, 10, 30]
 
 lo = logging.getLogger("rina")
+lo.setLevel(logging.DEBUG)
 rinaper_re = re.compile(r"Receiver\s*\d*\s*[\d\.]*\s*([\d\.]*)")
 
 def plot(result_rina, result_ip, title) -> dict:
@@ -72,10 +76,7 @@ def line_datarate_test():
     global line_result_rina
     global line_result_ip
 
-    #test_sizes = [2, 5]
-    test_sizes = [2, 5, 10, 20]
-
-    for size in test_sizes:
+    for size in LINE_SIZES:
         lo.info(f"Running datarate test with line topology with {size} nodes.")
         rina.cleanup()
         rina.create_networknamespaces(size)
@@ -100,6 +101,35 @@ def line_datarate_test():
     return (plot(line_result_rina, line_result_ip, 'Packet loss Line topology'))
 
 
+line_reno_result_ip = {}
+
+def line_datarate_reno_test():
+    global line_reno_result_ip
+
+    for size in LINE_SIZES:
+        lo.info(f"Running datarate RENO test with line topology with {size} nodes.")
+        rina.cleanup()
+        rina.create_networknamespaces(size)
+        rina.create_line_topology(size)
+        time.sleep(size)
+        package_loss = LOSS_START
+        rina.run('tc', 'qdisc', 'add', 'dev', f'veth{size - 1}-{size - 2}', 'root', 'netem', 'loss', str(package_loss)+'%', netns=f'node{size - 1}')
+    
+        while package_loss <= LOSS_MAX:
+            rina.run('tc', 'qdisc', 'change', 'dev', f'veth{size - 1}-{size - 2}', 'root', 'netem', 'loss', str(package_loss)+'%', netns=f'node{size - 1}')
+           
+            result_ip, _ = datarate.datarate_test(f'node{size - 1}', 'node0', '10.0.0.1', tcp_congestion_algo='reno', skip_rina=True)          
+
+            line_reno_result_ip[f'{size}:{package_loss}'] = result_ip
+
+            #print(f"RESULT IP: {line_result_ip[f'{size}:{package_loss}']}")
+            #print(f"RESULT RINA: {line_result_rina[f'{size}:{package_loss}']}")
+
+            package_loss += LOSS_INCREASE
+
+    return (plot(line_result_rina, line_reno_result_ip, 'Packet loss Line topology RENO'))
+
+
 # Fully meshed
 mesh_result_rina = {}
 mesh_result_ip = {}
@@ -108,10 +138,7 @@ def mesh_datarate_test():
     global mesh_result_rina
     global mesh_result_ip
 
-    #test_sizes = [3, 6]
-    test_sizes = [5, 10, 30]
-
-    for size in test_sizes:
+    for size in MESH_SIZES:
         lo.info(f"Running datarate test with fully meshed topology with {size} nodes.")
 
         rina.cleanup()
@@ -148,10 +175,7 @@ def redundant_datarate_test():
     global redundant_result_rina
     global redundant_result_ip
 
-    #test_sizes = [8]
-    test_sizes = [5, 10, 30]
-
-    for size in test_sizes:
+    for size in REDUNDANT_SIZES:
         lo.info(f"Running datarate test with Redundant topology with {size} nodes.")
 
         rina.cleanup()
@@ -193,6 +217,7 @@ def main() -> list:
     test_results = []
     rina.load_rlite()
     test_results.append(line_datarate_test())
+    test_results.append(line_datarate_reno_test())
     test_results.append(mesh_datarate_test())
     test_results.append(redundant_datarate_test())
     rina.cleanup()
